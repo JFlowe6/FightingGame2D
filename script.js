890694822016d4877c0fcea4d3d9e1318dc3d079
Jack Lowe <jacklowe@live.com>
2022-07-13 17:30:26 +0100

    Created sprites and added velocity, mainly downforce/gravity to get to grips with it Need to focus on movement now

--- script.js
+++ script.js
@@ -6,29 +6,61 @@ canvas.height = 576
 
 cTex.fillRect(0,0,canvas.width, canvas.height)
 
+const gravity = 0.2
+
 class Sprite {
-    constructor(position){
+    constructor({position, velocity}){
         this.position = position
+        this.velocity = velocity
+        this.height = 150
     }
 
     draw(){
         cTex.fillStyle = 'red'
-        cTex.fillRect(this.position.x, this.position.y, 50, 150)
+        cTex.fillRect(this.position.x, this.position.y, 50, this.height)
+    }
+
+    update(){
+        this.draw()
+        this.position.y += this.velocity.y
+
+        if (this.position.y + this.height + this.velocity.y >= canvas.height){
+            this.velocity.y = 0
+        } else{
+            this.velocity.y += gravity
+        }
     }
 }
 
 const player = new Sprite({
-    x: 0,
-    y: 0
-})
-
-player.draw()
+    position: {
+        x: 0,
+        y: 0
+    },
+    velocity: {
+        x: 0,
+        y: 0
+}})
 
 const enemy = new Sprite({
-    x: 974,
-    y: 0
+    position: {
+        x: 974,
+        y: 0
+    },
+    velocity: {
+        x: 0,
+        y: 0
+}
 })
 
-enemy.draw()
+// console.log(player)
 
-console.log(player)
+function movement() {
+    window.requestAnimationFrame(movement)
+    cTex.fillStyle = 'black'
+    cTex.fillRect(0, 0, canvas.width, canvas.height)
+    player.update()
+    enemy.update()
+}
+
+movement()
